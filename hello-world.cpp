35cc04d87b372d83f147bd129f8fb7df7471eb2c
careroms <wasu.github@gmail.com>
2026-07-17 11:12:52 +0700

    Fix bug

--- hello-world.cpp
+++ hello-world.cpp
@@ -15,7 +15,7 @@ int main() {
 
     std::time_t server_time = std::chrono::system_clock::to_time_t(now);
     std::tm* time_info = std::localtime(&server_time);
-    std::cout << "Server Time: " << std::put_time(time_info, "%Y-%m%d %H:%M:%S") << "\n";
+    std::cout << "Server Time: " << std::put_time(time_info, "%Y-%m%d %H:%M:%S") << std::endl;
   }
   
   return 0;
